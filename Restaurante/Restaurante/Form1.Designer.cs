6c824838d6c182af4a1c8d4cce6a878e28b5d333
Mayerli Azucena Montalvo <87994175+Mayerli2003@users.noreply.github.com>
2022-09-07 08:25:19 -0600

    Ejercicios

---
+++ Restaurante/Restaurante/Form1.Designer.cs
@@ -0,0 +1,435 @@
+﻿namespace Restaurante
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btneditar = new System.Windows.Forms.Button();
+            this.btnRegistar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textEdad = new System.Windows.Forms.TextBox();
+            this.comboGenero = new System.Windows.Forms.ComboBox();
+            this.textApellido = new System.Windows.Forms.TextBox();
+            this.textNombre = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Apellido = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rtbRegistros = new System.Windows.Forms.RichTextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.textBuscar = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comboDia = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.comboMes = new System.Windows.Forms.ComboBox();
+            this.comboYear = new System.Windows.Forms.ComboBox();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.btnEliminar);
+            this.panel1.Controls.Add(this.btneditar);
+            this.panel1.Controls.Add(this.btnRegistar);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Controls.Add(this.rtbRegistros);
+            this.panel1.Controls.Add(this.btnBuscar);
+            this.panel1.Controls.Add(this.textBuscar);
+            this.panel1.ForeColor = System.Drawing.Color.Transparent;
+            this.panel1.Location = new System.Drawing.Point(0, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 448);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.BackColor = System.Drawing.Color.Black;
+            this.btnEliminar.Location = new System.Drawing.Point(679, 381);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(93, 39);
+            this.btnEliminar.TabIndex = 6;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = false;
+            // 
+            // btneditar
+            // 
+            this.btneditar.BackColor = System.Drawing.Color.Black;
+            this.btneditar.Location = new System.Drawing.Point(533, 381);
+            this.btneditar.Name = "btneditar";
+            this.btneditar.Size = new System.Drawing.Size(93, 39);
+            this.btneditar.TabIndex = 5;
+            this.btneditar.Text = "Editar";
+            this.btneditar.UseVisualStyleBackColor = false;
+            // 
+            // btnRegistar
+            // 
+            this.btnRegistar.BackColor = System.Drawing.Color.Black;
+            this.btnRegistar.Location = new System.Drawing.Point(366, 381);
+            this.btnRegistar.Name = "btnRegistar";
+            this.btnRegistar.Size = new System.Drawing.Size(93, 39);
+            this.btnRegistar.TabIndex = 4;
+            this.btnRegistar.Text = "Registrar";
+            this.btnRegistar.UseVisualStyleBackColor = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboYear);
+            this.groupBox1.Controls.Add(this.comboMes);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.comboDia);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textEdad);
+            this.groupBox1.Controls.Add(this.comboGenero);
+            this.groupBox1.Controls.Add(this.textApellido);
+            this.groupBox1.Controls.Add(this.textNombre);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.Apellido);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(366, 77);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(406, 298);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Datos a registrar";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(24, 165);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Edad";
+            // 
+            // textEdad
+            // 
+            this.textEdad.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textEdad.Location = new System.Drawing.Point(112, 159);
+            this.textEdad.Name = "textEdad";
+            this.textEdad.Size = new System.Drawing.Size(265, 22);
+            this.textEdad.TabIndex = 8;
+            // 
+            // comboGenero
+            // 
+            this.comboGenero.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboGenero.FormattingEnabled = true;
+            this.comboGenero.Location = new System.Drawing.Point(112, 116);
+            this.comboGenero.Name = "comboGenero";
+            this.comboGenero.Size = new System.Drawing.Size(265, 24);
+            this.comboGenero.TabIndex = 7;
+            // 
+            // textApellido
+            // 
+            this.textApellido.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textApellido.Location = new System.Drawing.Point(112, 71);
+            this.textApellido.Name = "textApellido";
+            this.textApellido.Size = new System.Drawing.Size(265, 22);
+            this.textApellido.TabIndex = 4;
+            // 
+            // textNombre
+            // 
+            this.textNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textNombre.Location = new System.Drawing.Point(112, 29);
+            this.textNombre.Name = "textNombre";
+            this.textNombre.Size = new System.Drawing.Size(265, 22);
+            this.textNombre.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(86, 195);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(232, 16);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Fecha desde que visita el restaorante";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(21, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Genero";
+            // 
+            // Apellido
+            // 
+            this.Apellido.AutoSize = true;
+            this.Apellido.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Apellido.Location = new System.Drawing.Point(21, 75);
+            this.Apellido.Name = "Apellido";
+            this.Apellido.Size = new System.Drawing.Size(57, 16);
+            this.Apellido.TabIndex = 1;
+            this.Apellido.Text = "Apellido";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // rtbRegistros
+            // 
+            this.rtbRegistros.Location = new System.Drawing.Point(27, 77);
+            this.rtbRegistros.Name = "rtbRegistros";
+            this.rtbRegistros.Size = new System.Drawing.Size(313, 343);
+            this.rtbRegistros.TabIndex = 2;
+            this.rtbRegistros.Text = "";
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnBuscar.ForeColor = System.Drawing.Color.Black;
+            this.btnBuscar.Location = new System.Drawing.Point(588, 13);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 32);
+            this.btnBuscar.TabIndex = 1;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = false;
+            // 
+            // textBuscar
+            // 
+            this.textBuscar.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBuscar.ForeColor = System.Drawing.Color.White;
+            this.textBuscar.Location = new System.Drawing.Point(69, 13);
+            this.textBuscar.Multiline = true;
+            this.textBuscar.Name = "textBuscar";
+            this.textBuscar.Size = new System.Drawing.Size(476, 32);
+            this.textBuscar.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(142, 230);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(11, 16);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "/";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(281, 230);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(11, 16);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "/";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(89, 251);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(23, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Dia";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // comboDia
+            // 
+            this.comboDia.FormattingEnabled = true;
+            this.comboDia.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30",
+            "31"});
+            this.comboDia.Location = new System.Drawing.Point(81, 224);
+            this.comboDia.Name = "comboDia";
+            this.comboDia.Size = new System.Drawing.Size(55, 21);
+            this.comboDia.TabIndex = 16;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(195, 251);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(27, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Mes";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(310, 251);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(26, 13);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Año";
+            // 
+            // comboMes
+            // 
+            this.comboMes.FormattingEnabled = true;
+            this.comboMes.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12"});
+            this.comboMes.Location = new System.Drawing.Point(198, 227);
+            this.comboMes.Name = "comboMes";
+            this.comboMes.Size = new System.Drawing.Size(55, 21);
+            this.comboMes.TabIndex = 19;
+            // 
+            // comboYear
+            // 
+            this.comboYear.FormattingEnabled = true;
+            this.comboYear.Items.AddRange(new object[] {
+            "1999",
+            "2000",
+            "2001",
+            "2002",
+            "2003",
+            "2004",
+            "2005",
+            "2006",
+            "2007",
+            "2008",
+            "2009",
+            "2010",
+            "2011",
+            "2012",
+            "2013",
+            "2014",
+            "2015",
+            "2016",
+            "2017",
+            "2018",
+            "2019",
+            "2020",
+            "2021",
+            "2022",
+            "2023"});
+            this.comboYear.Location = new System.Drawing.Point(313, 224);
+            this.comboYear.Name = "comboYear";
+            this.comboYear.Size = new System.Drawing.Size(55, 21);
+            this.comboYear.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Restaurante.Properties.Resources._1543853044_168056_1544110723_sumario_normal;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.TextBox textBuscar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox rtbRegistros;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btneditar;
+        private System.Windows.Forms.Button btnRegistar;
+        private System.Windows.Forms.ComboBox comboGenero;
+        private System.Windows.Forms.TextBox textApellido;
+        private System.Windows.Forms.TextBox textNombre;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Apellido;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textEdad;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboYear;
+        private System.Windows.Forms.ComboBox comboMes;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox comboDia;
+    }
+}
+
---
+++ Restaurante/Restaurante/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Restaurante
+{
+    struct Fecha
+    {
+        public int dia;
+        public int mes;
+        public int year;
+
+    }
+    struct Persona
+    {
+        public string nombre;
+        public string apellido;
+        public string genero;
+        public string edad;
+        public Fecha FechaDeVisita;
+
+    }
+
+    public partial class Form1 : Form
+    {
+        //Declaracion  del arreglo tipo estructura 1
+
+        Persona[] Per = new Persona[100];
+
+        //Declaracion  del arreglo tipo estructura 2
+
+        Fecha[] fec = new Fecha[100];
+
+        //variables de uso global
+        int indice = 0, iModificar, indice1 = 0;
+        public Form1()
+        {  
+            InitializeComponent();
+        }
+
+        //Metodo para limpiar  los campos de texto
+        void LimpiarCampos()
+        {
+            textNombre.Text = "";
+            textApellido.Text = "";
+            comboGenero.Text = "";
+            textEdad.Text = "";
+            comboDia.Text = "";
+            comboMes.Text = "";
+            comboYear.Text = "";
+
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        void Registrar()
+        {
+            try
+            {
+                if (textNombre.Text != "" && textApellido.Text != "" && comboGenero.Text != "" && textEdad.Text != "")
+                {
+                    Per[indice].nombre = textNombre.Text;
+                    Per[indice].apellido = textApellido.Text;
+                    Per[indice].genero = Convert.ToString(comboGenero.SelectedItem);
+                    Per[indice].edad = textEdad.Text;
+                    Per[indice].FechaDeVisita.dia= Convert.ToInt32(comboDia.Text);
+                    Per[indice].FechaDeVisita.mes = Convert.ToInt32(comboMes.Text);
+                    Per[indice].FechaDeVisita.year = Convert.ToInt32(comboYear.Text);
+
+                }
+                else
+                {
+
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error al cargar el registro del libro", e.Message);
+
+            }
+           
+        }
+        //Metodo para mostrar los registros
+        void MostarRegistros()
+        {
+            try
+            {
+                string PersonaLista = "";
+                for (int i = 0; i < indice; i++)
+                {
+                    PersonaLista = PersonaLista + i + " - " + Per[i].nombre + " " + Per[i].apellido + " " + Per[i].genero + " " + Per[i].edad +"" +Per[i].FechaDeVisita.dia+"" + Per[i].FechaDeVisita.mes+""+"\n";
+
+                }
+                rtbRegistros.Text = PersonaLista;
+                
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Ocurrio un error al cargar los libros ", e.Message);
+
+            }
+        }
+    }
+}
